4523aeed85ffa9f073b7affe75063a6254e4ce8c
amaravathieluru <atieluru@gmail.com>
2026-03-10 18:22:37 +0530

    Update CmsApplication.java

--- cms/src/main/java/com/complaint/cms/CmsApplication.java
+++ cms/src/main/java/com/complaint/cms/CmsApplication.java
@@ -2,12 +2,12 @@ package com.complaint.cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class CmsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(CmsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class, args);
+    }
 }
